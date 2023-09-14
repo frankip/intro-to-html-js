2154b5b1d2631b437f4173d676f713e771027ff2
Frank <francis.kipchumba@moringaschool.com>
2023-09-14 10:40:00 +0300

    set up the projects

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// console.log('we are working')
+
+// constant
+// let number=[a,b,c]
+let greeting = "hello"
+const user1 = "John kiriamiti"
+const user2= "makmende"
+let evaluate= 10>5
+// assignmenthttps://moringa.instructure.com/conversations
+// lookup
+
+userName = 'makmende'
+phoneNumber = 0722000000
+emailAddress = "john@gmail.com"
+// summation = (10+4-8/5(9*8))
+
+// console.log(userName)
+
+
+// user2 === userName
+
+
+
+// let
+// const
+// var
+
+
+// Javascript datatypes
+// numbers
+let add = 9 +6
+let multiply = 1.3 * 5.7 
+
+// strings
+a= "moringa development"
+b='software developer journey '
+c=`we are learning javascript  with ${5*8}`
+
+// console.log(a);
+// console.log(b);
+// console.log(a.toUpperCase());
+// boolean
+// true 
+// false
+console.log(user1 !== userName);
+// object
+// arrays
+// undefined
+// null
+
+
